86965bd0cbfa991701081791ce06c805881c6b67
Ns Sidd <76404154+nssidd0@users.noreply.github.com>
2020-12-21 21:47:06 +0530

    Update gsend.php

--- gsend.php
+++ gsend.php
@@ -1,20 +1,20 @@
 <?php
 // variable setting
-$name = $_REQUEST['Name'];
-$email = $_REQUEST['Email'];
-$subject = $_REQUEST['Uubjest'];
-$message = $_REQUEST['Message'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subjest'];
+$message = $_REQUEST['message'];
 
 //check input fields
 if (empty($name) || empty($email) || empty($subject) || empty($message))
 {
-	echo "Please fill all the fields";
+	echo ('Please fill all the fields');
 }
 else
 {
-	mail("nssidd0@gmail.com", "Web MSG", $message , "From: $name <$email>");
-	echo "<script type='text/javascript'>alert('your message sent successfully');
-	window.history.log(-1);
+	mail("nssidd0@gmail.com", "Email form", $message , "From: $name <$email>");
+	echo "<script type='text/javascript'>alert('Message Sent Successfully!...');
+	window.history.go(-1);
 	</script>";
 }
 ?>
