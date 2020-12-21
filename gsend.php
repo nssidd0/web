75c7b9ebaee7d7640118c09b40b35f4177bf3627
Ns Sidd <76404154+nssidd0@users.noreply.github.com>
2020-12-21 16:19:03 +0530

    Add files via upload

---
+++ gsend.php
@@ -0,0 +1,20 @@
+<?php
+	if (isset($_POST['submit'])) {
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$msg=$_POST['msg'];
+
+		$to='nssidd0@gmail.com';
+		$subject='Form Submission';
+		$message="Name: ".$name."\n"."Subject: ".$subject."\n". "Wrote the following: "."\n\n".$msg;
+		$headers="Form: ".$email;
+
+		if(mail($to, $subject, $message, $header)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
