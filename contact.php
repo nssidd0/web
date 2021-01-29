a3ff9c7e4015baa3b7acf5f2cd2686c20e50484a
Ns Sidd <76404154+nssidd0@users.noreply.github.com>
2021-01-29 21:31:39 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 		$subject=$_POST['subject'];
 		$msg=$_POST['msg'];
 
-		$to='contactus@friendsresorts.com'; // Receiver Email ID, Replace with your email ID
+		$to='nssidd0gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
 		$message="Name :".$name."\n"."Subject :".$subject."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
