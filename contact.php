0fa0ca88c0575c3a96f22e22fbc57c712632d75e
Ns Sidd <76404154+nssidd0@users.noreply.github.com>
2021-01-29 21:30:12 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,20 @@
 <?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$msg=$_POST['msg'];
 
-$to = nssidd0@gmail.com";
-$subject = "GRAPHIC DESIGN";
-$message = "i need in 20 days.";
-$from = "janamk470@gmail.com";
-$headers = "From: $from";
+		$to='contactus@friendsresorts.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Subject :".$subject."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
 
-if(mail($to,$subject,$message,$headers)){
-	echo "Mail Sent Successfully.";
-}else{
-	echo "Email Sent Failed.";
-}
-
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
 ?>
